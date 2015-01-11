e0af260277de25e4098fdc065fecf0f7c19a60be
balgjlawson <g.j.lawson99@gmail.com>
2015-01-11 15:09:28 -0800

    Update Robot.java

--- kicker-bot-practice/Robot.java
+++ kicker-bot-practice/Robot.java
@@ -15,22 +15,37 @@ public class Robot extends SampleRobot {
 	
     RobotDrive robotDrive;
     Joystick stick;
+    Joystick otherStick;
 
     // Channels for the wheels
-    final Talon frontLeftChannel	= new Talon(2);
+   
+ 
+    
+    
+    
+    final Talon frontLeftChannel = new Talon(2);
     final Talon rearLeftChannel	= new Talon (3);
-    final Talon frontRightChannel	= new Talon (1);
-    final Talon rearRightChannel	= new Talon (4);
+    final Talon frontRightChannel = new Talon (1);
+    final Talon rearRightChannel = new Talon (4);
+
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
-
+    final int otherJoystickChannel = 1;
+    double  frontLeftVoltage;
+    double rearLeftVoltage;
+    double frontRightVoltage;
+    double rearRightVoltage;
     public Robot() {
     
-        robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
+    	
+    robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
     	// you may need to change or remove this to match your robot
 
         stick = new Joystick(joystickChannel);
+        otherStick = new Joystick(otherJoystickChannel);
+        
+
     }
         
 
@@ -40,10 +55,25 @@ public class Robot extends SampleRobot {
     public void operatorControl() {
         robotDrive.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-        	
+        	   double robotSpeedFL = stick.getY();
+        	    double robotAngleFL = otherStick.getZ();
+        	    double robotStrafeFL = stick.getX();
+        	    
+        	    frontLeftVoltage =robotSpeedFL*Math.sin(robotAngleFL+(Math.PI/4)+robotStrafeFL);
+                rearLeftVoltage = robotSpeedFL*Math.cos(robotAngleFL+(Math.PI/4)+robotStrafeFL);
+                frontRightVoltage = robotSpeedFL*Math.cos(robotAngleFL+(Math.PI/4)-robotStrafeFL);
+                rearRightVoltage = robotSpeedFL*Math.sin(robotAngleFL+(Math.PI/4)-robotStrafeFL);
+            	
+        	    
+            frontLeftChannel.set(frontLeftVoltage);
+            rearLeftChannel.set(rearLeftVoltage);
+            frontRightChannel.set(frontRightVoltage);
+            rearLeftChannel.set(rearLeftVoltage);
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
-        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-            robotDrive.mecanumDrive_Cartesian(stick.getZ(), stick.getY()/2, -stick.getX()/2, 0);
+        	// This sample does not use fi
+            eld-oriented drive, so the gyro input is set to zero.
+            //Z and X are inverted because the joystick is stupid.
+        	robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), otherStick.getX(), 0);
             
             Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
         }
